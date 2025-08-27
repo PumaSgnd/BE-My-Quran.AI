c70309ad154763b677a22d2a11b5a8019e985f8b
PumaSgnd <bomavan12@gmail.com>
2025-08-27 11:18:40 +0700

    feat: Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
-// src/api/index.js
-const serverless = require('serverless-http');
-const app = require('../index');
-const handler = serverless(app);
+// // src/api/index.js
+// const serverless = require('serverless-http');
+// const app = require('../index');
+// const handler = serverless(app);
 
-module.exports = (req, res) => handler(req, res);
+// module.exports = (req, res) => handler(req, res);
+
+// src/api/index.js
+const app = require('../index');
+
+// Vercel Serverless Functions bisa langsung consume Express app
+module.exports = app;
