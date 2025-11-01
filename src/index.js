f37cb0893b3af55d857632c7c3ddda9bef9c04fa
PumaSgnd <bomavan12@gmail.com>
2025-11-01 18:13:53 +0700

    Cancel Add Image Temukan

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ app.use('/api/chat', require('./routes/chat.routes.js'));
 app.use("/api/doa", require('./routes/doa.routes.js'));
 app.use('/api/doa-user', require('./routes/doaUser.routes.js'));
 app.use('/api/prayer', require('./routes/prayer.routes.js'));
-app.use('/api/prayer-weekly', require('./routes/weeklyPrayer.routes.js'));
+app.use('/api/prayer-weekly', require('./routes/weeklyPrayer.routes.js'))
 
 app.use((_req, res) => {
     res.status(404).json({ status: 'Error', message: 'Resource tidak ditemukan' });
