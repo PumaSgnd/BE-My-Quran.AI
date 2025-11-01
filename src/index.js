45b9bf631a6bb4403a08df2ce934160e7ff36f05
PumaSgnd <bomavan12@gmail.com>
2025-11-01 17:39:05 +0700

    Update Image Temukan

--- src/index.js
+++ src/index.js
@@ -125,7 +125,8 @@ app.use('/api/prayer', require('./routes/prayer.routes.js'));
 app.use('/api/prayer-weekly', require('./routes/weeklyPrayer.routes.js'));
 
 // temukan image
-app.use('/uploads', express.static('public/uploads'));
+app.use(express.static(path.join(rootDir, 'public')));
+app.use('/uploads', express.static(path.join(rootDir, 'public/uploads')));
 
 app.use((_req, res) => {
     res.status(404).json({ status: 'Error', message: 'Resource tidak ditemukan' });
