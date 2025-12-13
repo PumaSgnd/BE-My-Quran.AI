281352b2698736c189758e75decaffca9185af9c
PumaSgnd <bomavan12@gmail.com>
2025-12-13 10:12:01 +0700

    Add API Hadist

--- src/models/hadith.model.js
+++ src/models/hadith.model.js
@@ -2,24 +2,34 @@ const pool = require('../config/db');
 
 module.exports = {
     async findById(id) {
-        const result = await pool.query(
-            `SELECT h.id, h.book, h.number, h.arab, h.indo,
-            c.id AS category_id, c.name AS category
-     FROM hadith h
-     LEFT JOIN categories c ON c.id = h.category_id
-     WHERE h.id = $1
-     LIMIT 1`,
-            [id]
-        );
-
+        const result = await pool.query(`
+            SELECT 
+            h.id, 
+            h.book_id,
+            h.number, 
+            h.arab, 
+            h.indo,
+            c.id AS category_id, 
+            c.name AS category
+            FROM hadith h
+            LEFT JOIN categories c ON c.id = h.category_id
+            WHERE h.id = $1
+            LIMIT 1
+        `, [id]);
         return result.rows[0];
     },
 
     async findByBook(slug) {
         const result = await pool.query(`
-            SELECT h.id, h.book_id, h.number, h.arab, h.indo,
-                c.id AS category_id, c.name AS category
-            FROM hadiths h
+            SELECT 
+            h.id, 
+            h.book_id,
+            h.number, 
+            h.arab, 
+            h.indo,
+            c.id AS category_id, 
+            c.name AS category
+            FROM hadith h
             JOIN books b ON b.id = h.book_id
             LEFT JOIN categories c ON c.id = h.category_id
             WHERE b.slug = $1
@@ -35,12 +45,11 @@ module.exports = {
             b.slug,
             b.title,
             COUNT(h.id)::int AS count
-            FROM hadiths h
+            FROM hadith h
             JOIN books b ON b.id = h.book_id
             GROUP BY b.id, b.slug, b.title
             ORDER BY count DESC
         `);
         return result.rows;
     }
-
 };
