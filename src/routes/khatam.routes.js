9d9ca9dc13e4d1a368e2f0cb0184ce973943b7a1
PumaSgnd <bomavan12@gmail.com>
2026-02-12 23:47:20 +0700

    Update Khatam

--- src/routes/khatam.routes.js
+++ src/routes/khatam.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const khatam = require('../controllers/khatam.controller');
+const { isLoggedIn } = require('../middlewares/auth.middleware');
+
+router.use(isLoggedIn);
 
 router.post('/create', khatam.createPlan);
 router.get('/active', khatam.getActivePlan);
